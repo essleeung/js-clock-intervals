7bdbc172f204451891affb4de782a800e20b37fc
essleeung <essleeung@gmail.com>
2020-03-24 11:38:54 -0400

    bonus clock, works but takes a bit of time to load?

--- clock.js
+++ clock.js
@@ -1,26 +1,51 @@
+// const secHand = document.getElementById("second");
+// const minHand = document.getElementById("minute");
+// const hrHand = document.getElementById("hour");
+
+
+// function secRotation() {
+//     let secDegree = (360/60) * seconds;
+//     secHand.style.transform = "rotate(" + secDegree + "deg)"
+// }
+// setInterval(secRotation, 1000)
+
+// function minRotation() {
+//     let minDegree = ((360/60) * minutes) + (360/60 * seconds)/60;
+//     minHand.style.transform = "rotate(" + minDegree + "deg)"
+// }
+
+
+// function hourRotation() {
+//     let hourDegree = ((360/12) * hour) + ((360/60 * minutes)/12);
+//     hrHand.style.transform = "rotate(" + hourDegree + "deg)"
+// }
+var today = new Date();
+var seconds = today.getSeconds();
+var minutes = today.getMinutes();
+var hr = today.getHours();
 var sec = document.getElementById("second");
 var min = document.getElementById("minute");
 var hour = document.getElementById("hour");
-var secDegree = 0;
-var minDegree = 0;
-var hourDegree = 0;
+var secDegree = seconds/60*360;
+var minDegree = minutes/60*360;
+var hourDegree = hr/12*360;
 
 function secRotation() {
-    degrees = Math.floor((1/60)*360);
+    degrees = (1/60)*360;
     secDegree += degrees;
     sec.style.transform = "rotate(" + secDegree + "deg)"
 }
 setInterval(secRotation, 1000);
 
 function minRotation() {
-    degrees = Math.floor((1/60)*360);
+    degrees = (1/60)*360;
     minDegree += degrees;
     min.style.transform = "rotate(" + minDegree + "deg)"
 }
 setInterval(minRotation, 60000);
 
 function hourRotation() {
-    degrees = Math.floor((1/12)*360);
+    degrees = (1/12)*360;
     hourDegree += degrees;
     hour.style.transform = "rotate(" + hourDegree + "deg)"
 }
